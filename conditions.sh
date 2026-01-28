d6d6cedc9a3bd5d959f72e287ef747e5a88d597d
Bhavitha <lbhavitha342@gmail.com>
2026-01-28 21:36:14 +0530

    added one more line

--- conditions.sh
+++ conditions.sh
@@ -4,8 +4,8 @@ NUM=25
 
 if [ $NUM -gt 20 ]; then
     echo "yes, num is greater than 20"
-# elif [ $NUM -lt 20 ]; then
-#         echo "yes, num is less than 20"
+elif [ $NUM -lt 20 ]; then
+        echo "yes, num is less than 20"
 else
-    echo "no. is not gt 20"
+    echo "no. is eq to 20"
 fi 
