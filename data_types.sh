570e882779195ea22a97701176bf317f3f59fbe1
Bhavitha <lbhavitha342@gmail.com>
2026-01-28 21:23:29 +0530

    added one more line

--- data_types.sh
+++ data_types.sh
@@ -12,4 +12,7 @@
 #Array(list of items)
 FRUITS=("Apple" "Banana" "Guava")
 
-echo "FRUITS are: ${FRUITS[@]}"
+# echo "FRUITS are: ${FRUITS[@]}"
+echo "FRUITS 1: ${FRUITS[0]}"
+echo "FRUITS 2: ${FRUITS[1]}"
+echo "FRUITS 3: ${FRUITS[2]}"
