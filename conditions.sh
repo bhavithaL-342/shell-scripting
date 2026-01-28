7ddd8e3f215c4f699734031e70217dfe1f71d5a1
Bhavitha <lbhavitha342@gmail.com>
2026-01-28 21:31:40 +0530

    added one more line

--- conditions.sh
+++ conditions.sh
@@ -6,6 +6,6 @@ if [ $NUM -gt 20 ]; then
     echo "yes, num is greater than 20"
 elif [ $NUM -lt 20 ]; then
         echo "yes, num is less than 20"
-else [ $NUM -eq 20 ]; then
-        echo "yes, num is equal to 20"
+else
+    echo "yes, num is equal to 20"
 
