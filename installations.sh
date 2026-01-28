eb0d92bfea34e61dbfea541896768054fe0c583e
Bhavitha <lbhavitha342@gmail.com>
2026-01-28 21:57:42 +0530

    added one more line

---
+++ installations.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]; then 
+    echo "Please run this script with root user access"
+    exit 1
+fi
+
+echo "installing nginx..."
+dnf install nginx -y
+
