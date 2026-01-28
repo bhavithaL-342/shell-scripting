abd7aecb84cb138ac22dc3fe0b01fd29d803b6df
Bhavitha <lbhavitha342@gmail.com>
2026-01-28 19:48:07 +0530

    added one more line

--- variables-01.sh
+++ variables-01.sh
@@ -9,10 +9,19 @@
 # echo "Bhavitha: I'm practicing shell scripting"
 
 
-PERSON1=Lakshmi
-PERSON2=Bhavitha
+# PERSON1=Lakshmi
+# PERSON2=Bhavitha
 
-echo "$PERSON1: Hi Bhavitha"
+# echo "$PERSON1: Hi $PERSON2"
+# echo "$PERSON2: Hi $PERSON1, How are you"
+# echo "$PERSON1: I'm good, what are you doing"
+# echo "$PERSON2: I'm practicing shell scripting"
+
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2"
 echo "$PERSON2: Hi $PERSON1, How are you"
 echo "$PERSON1: I'm good, what are you doing"
 echo "$PERSON2: I'm practicing shell scripting"
