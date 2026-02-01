816c63ac284f3871fe60960547f312e197fffc4e
BhavithaL <lbhavitha342@gmail.com>
2026-02-01 21:41:33 +0530

    commit all

--- colour-codes.sh
+++ colour-codes.sh
@@ -3,7 +3,7 @@
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-N="\e[34m" #normal
+N="\e[0m" #normal
 
 USERID=$(id -u)
 
@@ -12,15 +12,15 @@ LOGS_FILE="/var/log/shell-script/$0.log"
 
 
 if [ $USERID -ne 0 ]; then
-    echo "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
     exit 1
 fi
 
 VALIDATE(){
 if [ $1 -ne 0 ]; then 
-    echo "$2 is $R FAILURE $N" | tee -a $LOGS_FILE #tee -> prints log/output on screen and in log file, -a is append(without override)
+    echo -e "$2 is $R FAILURE $N" | tee -a $LOGS_FILE #tee -> prints log/output on screen and in log file, -a is append(without override)
 else 
-    echo "$2 is $G SUCCESS $N" | tee -a $LOGS_FILE
+    echo -e "$2 is $G SUCCESS $N" | tee -a $LOGS_FILE
 fi
 }
 
@@ -28,10 +28,10 @@ for package in $@ #sudo sh loops.sh nginx mysql nodejs
 do
     dnf list installed $package &>> $LOGS_FILE #will say whether the package already installed or not and run echo $?-> if 0 installed else no
     if [ $? -ne 0 ]; then
-        echo "$package not installed, $Y installing now $N"
+        echo -e "$package not installed, $Y installing now $N"
         dnf install $package -y &>> $LOGS_FILE
         VALIDATE $? "$package installation"
     else
-        echo "$package already installed, $Y skipping $N"
+        echo -e "$package already installed, $Y skipping $N"
     fi
 done
