cb6fc5814f06ec8ab1210ab4eeffb96c1e028ad3
BhavithaL <lbhavitha342@gmail.com>
2026-02-01 21:14:49 +0530

    commit all

--- loops-1.sh
+++ loops-1.sh
@@ -6,9 +6,6 @@ LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
 
 
-
-
-
 if [ $USERID -ne 0 ]; then
     echo "Please run this script with root user access" | tee -a $LOGS_FILE
     exit 1
