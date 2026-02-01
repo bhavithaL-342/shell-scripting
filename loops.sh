bfbf868fd6c8800d375b6f673482bb9c183780ee
BhavithaL <lbhavitha342@gmail.com>
2026-02-01 20:40:31 +0530

    commit all

---
+++ loops.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+VALIDATE(){
+if [ $1 -ne 0 ]; then 
+    echo "$2 is FAILURE" | tee -a $LOGS_FILE #tee -> prints log/output on screen and in log file, -a is append(without override)
+else 
+    echo "$2 is SUCCESS" | tee -a $LOGS_FILE
+fi
+}
+
+# dnf install nginx -y &>> $LOGS_FILE
+# VALIDATE $?, "Installing nginx"
+
+# dnf install mysql -y &>> $LOGS_FILE
+# VALIDATE $?, "Installing mysql"
+
+# dnf install nodejs -y &>> $LOGS_FILE
+# VALIDATE $?, "Installing nodejs"
+
+for package in $@ #sudo sh loops.sh nginx mysql nodejs
+do
+    dnf install $package -y 
+    VALIDATE $? "$package installation"
+done
