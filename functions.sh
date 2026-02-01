b7a629f76def6d8cd397e7df703c5bb0af41e271
BhavithaL <lbhavitha342@gmail.com>
2026-02-01 17:36:43 +0530

    commit all

--- functions.sh
+++ functions.sh
@@ -2,16 +2,16 @@
 
 USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
-    echo"Please run this script with root user access"
+    echo "Please run this script with root user access"
     exit 1
 fi
 
 echo "Installing nginx..."
 dnf install nginx -y
 
-if [ $? -ne 0 ]; then #to know whether (dnf install nginx -y) command sucsess or not
-    echo"Installing nginx is FAILURE"
+if [ $? -ne 0 ]; then #to know whether (dnf install nginx -y) command success or not
+    echo "Installing nginx is FAILURE"
 else 
-    echo"Installing nginx is SUCCESS"
+    echo "Installing nginx is SUCCESS"
 fi
 
